6f777cbc8c943a8c363cbd5ab036d1f821dee4bc
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-01-16 17:56:52 +0700

    Create plugins.js

---
+++ plugins.js
@@ -0,0 +1,15 @@
+<a style="display:none"href="https://www.backinfo.egov.balangankab.go.id/public/slot-deposit-pulsa/">https://www.backinfo.egov.balangankab.go.id/public/slot-deposit-pulsa/</a>
+<a style="display:none"href="https://www.backinfo.egov.balangankab.go.id/public/slot-deposit-dana/">https://www.backinfo.egov.balangankab.go.id/public/slot-deposit-dana/</a>
+<a style="display:none"href="http://sipessut.samarindakota.go.id/posdira/public/">http://sipessut.samarindakota.go.id/posdira/public/</a>
+<a style="display:none"href="https://www.menuresep.com/slot-deposit-pulsa/">https://www.menuresep.com/slot-deposit-pulsa/</a>
+<a style="display:none"href="http://fiadorestaurante.com/slot-online-terpercaya/">http://fiadorestaurante.com/slot-online-terpercaya/</a>
+<a style="display:none"href="https://mahapalabumi.peradaban.ac.id/slot-gacor-maxwin/">https://mahapalabumi.peradaban.ac.id/slot-gacor-maxwin/</a>
+<a style="display:none"href="https://bdimakassar.kemenperin.go.id/togel-online/">https://bdimakassar.kemenperin.go.id/togel-online/</a>
+<a style="display:none"href="https://mahapalabumi.peradaban.ac.id/togel-hari-ini/">https://mahapalabumi.peradaban.ac.id/togel-hari-ini/</a>
+<a style="display:none"href="https://mahapalabumi.peradaban.ac.id/slot-deposit-pulsa-10rb/">https://mahapalabumi.peradaban.ac.id/slot-deposit-pulsa-10rb/</a>
+<a style="display:none"href="https://mutu.uhamka.ac.id/slot-gacor-terbaru/">https://mutu.uhamka.ac.id/slot-gacor-terbaru/</a>
+<a style="display:none"href="https://www.cfc.filkom.unvicsorong.ac.id/slot-gacor-terbaru/">https://www.cfc.filkom.unvicsorong.ac.id/slot-gacor-terbaru/</a>
+<a style="display:none"href="https://simpul.cianjurkab.go.id/slot-gacor-maxwin/">https://simpul.cianjurkab.go.id/slot-gacor-maxwin/</a>
+<a style="display:none"href="https://www.menuresep.com/slot-deposit-pulsa-tanpa-potongan/">https://www.menuresep.com/slot-deposit-pulsa-tanpa-potongan/</a>
+<a style="display:none"href="http://sipessut.samarindakota.go.id/sipessut/slot-deposit-pulsa-10-ribu/">http://sipessut.samarindakota.go.id/sipessut/slot-deposit-pulsa-10-ribu/</a>
+<a style="display:none"href="https://mutu.uhamka.ac.id/slot-deposit-via-dana/">https://mutu.uhamka.ac.id/slot-deposit-via-dana/</a>
