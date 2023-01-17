539350b12a821269a2423ce4ca5045d6d2978957
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-01-17 18:27:36 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,6 +1,6 @@
 <a style="display:none"href="https://www.backinfo.egov.balangankab.go.id/public/slot-deposit-pulsa/">https://www.backinfo.egov.balangankab.go.id/public/slot-deposit-pulsa/</a>
 <a style="display:none"href="https://www.backinfo.egov.balangankab.go.id/public/slot-deposit-dana/">https://www.backinfo.egov.balangankab.go.id/public/slot-deposit-dana/</a>
-<a style="display:none"href="http://sipessut.samarindakota.go.id/posdira/public/">http://sipessut.samarindakota.go.id/posdira/public/</a>
+<a style="display:none"href="https://dpmfai.uhamka.ac.id/slot-deposit-dana-10-ribu/">https://dpmfai.uhamka.ac.id/slot-deposit-dana-10-ribu/</a>
 <a style="display:none"href="https://www.menuresep.com/slot-deposit-pulsa/">https://www.menuresep.com/slot-deposit-pulsa/</a>
 <a style="display:none"href="http://fiadorestaurante.com/slot-online-terpercaya/">http://fiadorestaurante.com/slot-online-terpercaya/</a>
 <a style="display:none"href="https://mahapalabumi.peradaban.ac.id/slot-gacor-maxwin/">https://mahapalabumi.peradaban.ac.id/slot-gacor-maxwin/</a>
@@ -11,5 +11,5 @@
 <a style="display:none"href="https://www.cfc.filkom.unvicsorong.ac.id/slot-gacor-terbaru/">https://www.cfc.filkom.unvicsorong.ac.id/slot-gacor-terbaru/</a>
 <a style="display:none"href="https://simpul.cianjurkab.go.id/slot-gacor-maxwin/">https://simpul.cianjurkab.go.id/slot-gacor-maxwin/</a>
 <a style="display:none"href="https://www.menuresep.com/slot-deposit-pulsa-tanpa-potongan/">https://www.menuresep.com/slot-deposit-pulsa-tanpa-potongan/</a>
-<a style="display:none"href="http://sipessut.samarindakota.go.id/sipessut/slot-deposit-pulsa-10-ribu/">http://sipessut.samarindakota.go.id/sipessut/slot-deposit-pulsa-10-ribu/</a>
 <a style="display:none"href="https://mutu.uhamka.ac.id/slot-deposit-via-dana/">https://mutu.uhamka.ac.id/slot-deposit-via-dana/</a>
+<a style="display:none"href="http://fiadorestaurante.com/slot-online-resmi/">http://fiadorestaurante.com/slot-online-resmi/</a>
