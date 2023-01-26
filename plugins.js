8af538fb22995b4b42f93e5f98b256b5a52e5925
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-01-26 12:16:09 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -2,6 +2,5 @@
 <a style="display:none"href="http://fiadorestaurante.com/slot-online-resmi/">http://fiadorestaurante.com/slot-online-resmi/</a>
 <a style="display:none"href="https://mahapalabumi.peradaban.ac.id/togel-resmi-terpercaya/">https://mahapalabumi.peradaban.ac.id/togel-resmi-terpercaya/</a>
 <a style="display:none"href="https://www.cfc.filkom.unvicsorong.ac.id/slot-gacor-gampang-maxwin/">https://www.cfc.filkom.unvicsorong.ac.id/slot-gacor-gampang-maxwin/</a>
-<a style="display:none"href="https://mutu.uhamka.ac.id/slot-gacor-gampang-maxwin/">https://mutu.uhamka.ac.id/slot-gacor-gampang-maxwin/</a>
 <a style="display:none"href="https://badung.bawaslu.go.id/slot-deposit-dana/">https://badung.bawaslu.go.id/slot-deposit-dana/</a>
 <a style="display:none"href="https://www.joyfulnoisenamibia.com/slot-deposit-via-dana-10-ribu/">https://www.joyfulnoisenamibia.com/slot-deposit-via-dana-10-ribu/</a>
