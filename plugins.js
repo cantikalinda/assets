bb529b179c4b6dd0ddbd0848f701bc7b94f398fd
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-02-06 16:16:48 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,3 +1,5 @@
 <a style="display:none"href="https://www.teknikkimia.itbmb.ac.id/slot-deposit-dana/">https://www.teknikkimia.itbmb.ac.id/slot-deposit-dana/</a>
 <a style="display:none"href="https://www.itbmb.ac.id/slot-deposit-pulsa/">https://www.itbmb.ac.id/slot-deposit-pulsa/</a>
 <a style="display:none"href="https://fikes.unigal.ac.id/casino-online/">https://fikes.unigal.ac.id/casino-online/</a>
+<a style="display:none"href="https://www.cmdr.ac.in/slot-deposit-via-dana-24-jam/">https://www.cmdr.ac.in/slot-deposit-via-dana-24-jam/</a>
+
