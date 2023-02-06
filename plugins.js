8b007bc8d7e4c4cda6c7aef3b0ff21d115843b1d
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-02-06 16:13:29 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,13 +1,3 @@
-<a style="display:none"href="https://simpul.cianjurkab.go.id/slot-gacor-maxwin/">https://simpul.cianjurkab.go.id/slot-gacor-maxwin/</a>
-<a style="display:none"href="http://fiadorestaurante.com/slot-online-resmi/">http://fiadorestaurante.com/slot-online-resmi/</a>
-<a style="display:none"href="https://mahapalabumi.peradaban.ac.id/togel-resmi-terpercaya/">https://mahapalabumi.peradaban.ac.id/togel-resmi-terpercaya/</a>
-<a style="display:none"href="https://www.cfc.filkom.unvicsorong.ac.id/slot-gacor-gampang-maxwin/">https://www.cfc.filkom.unvicsorong.ac.id/slot-gacor-gampang-maxwin/</a>
-<a style="display:none"href="https://badung.bawaslu.go.id/slot-deposit-dana/">https://badung.bawaslu.go.id/slot-deposit-dana/</a>
-<a style="display:none"href="https://www.joyfulnoisenamibia.com/slot-deposit-via-dana-10-ribu/">https://www.joyfulnoisenamibia.com/slot-deposit-via-dana-10-ribu/</a>
-<a style="display:none"href="https://www.cmdr.ac.in/slot-deposit-via-dana-24-jam/">https://www.cmdr.ac.in/slot-deposit-via-dana-24-jam/</a>
-<a style="display:none"href="https://teknikkimia.unsika.ac.id/slot-deposit-dana-10-ribu/">https://teknikkimia.unsika.ac.id/slot-deposit-dana-10-ribu/</a>
-<a style="display:none"href="https://www.itbmb.ac.id/slot-deposit-dana/">https://www.itbmb.ac.id/slot-deposit-dana/</a>
 <a style="display:none"href="https://www.teknikkimia.itbmb.ac.id/slot-deposit-dana/">https://www.teknikkimia.itbmb.ac.id/slot-deposit-dana/</a>
-<a style="display:none"href="https://teknikkimia.unsika.ac.id/slot-deposit-pulsa-tanpa-potongan/">https://teknikkimia.unsika.ac.id/slot-deposit-pulsa-tanpa-potongan/</a>
 <a style="display:none"href="https://www.itbmb.ac.id/slot-deposit-pulsa/">https://www.itbmb.ac.id/slot-deposit-pulsa/</a>
-
+<a style="display:none"href="https://fikes.unigal.ac.id/casino-online/">https://fikes.unigal.ac.id/casino-online/</a>
