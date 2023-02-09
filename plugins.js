adb37ef24326a9c5e7c06c588d9eef9196cbd7af
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-02-09 10:35:48 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,10 +1,5 @@
-<a style="display:none"href="https://www.teknikkimia.itbmb.ac.id/slot-deposit-dana/">https://www.teknikkimia.itbmb.ac.id/slot-deposit-dana/</a>
 <a style="display:none"href="https://www.itbmb.ac.id/slot-deposit-pulsa/">https://www.itbmb.ac.id/slot-deposit-pulsa/</a>
-<a style="display:none"href="https://fikes.unigal.ac.id/casino-online/">https://fikes.unigal.ac.id/casino-online/</a>
-<a style="display:none"href="https://www.cmdr.ac.in/slot-deposit-via-dana-24-jam/">https://www.cmdr.ac.in/slot-deposit-via-dana-24-jam/</a>
 <a style="display:none"href="https://teknikkimia.itbmb.ac.id/slot-deposit-pulsa-10-ribu/">https://teknikkimia.itbmb.ac.id/slot-deposit-pulsa-10-ribu/</a>
-<a style="display:none"href="https://fikes.unigal.ac.id/slot-gacor/">https://fikes.unigal.ac.id/slot-gacor/</a>
+<a style="display:none"href="https://fikes.unigal.ac.id/casino-online/">https://fikes.unigal.ac.id/casino-online/</a>
 <a style="display:none"href="https://fikes.unigal.ac.id/slot-deposit-pulsa/">https://fikes.unigal.ac.id/slot-deposit-pulsa/</a>
-<a style="display:none"href="https://fikes.unigal.ac.id/slot-deposit-dana/">https://fikes.unigal.ac.id/slot-deposit-dana/</a>
-<a style="display:none"href="https://sso.gianyarkab.go.id/assets/slot-gacor/">https://sso.gianyarkab.go.id/assets/slot-gacor/</a>
 <a style="display:none"href="https://teknikkimia.unsika.ac.id/slot-deposit-pulsa-tanpa-potongan/">https://teknikkimia.unsika.ac.id/slot-deposit-pulsa-tanpa-potongan/</a>
