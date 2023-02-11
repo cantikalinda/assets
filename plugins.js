6a2b8ae9dcf9b018946c46dffe86a0536596a85c
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-02-11 13:58:22 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,10 +1,9 @@
 <a style="display:none"href="https://www.itbmb.ac.id/slot-deposit-pulsa/">https://www.itbmb.ac.id/slot-deposit-pulsa/</a>
-<a style="display:none"href="https://teknikkimia.itbmb.ac.id/slot-deposit-pulsa-10-ribu/">https://teknikkimia.itbmb.ac.id/slot-deposit-pulsa-10-ribu/</a>
 <a style="display:none"href="https://fikes.unigal.ac.id/casino-online/">https://fikes.unigal.ac.id/casino-online/</a>
-<a style="display:none"href="https://fikes.unigal.ac.id/slot-deposit-pulsa/">https://fikes.unigal.ac.id/slot-deposit-pulsa/</a>
 <a style="display:none"href="https://teknikkimia.unsika.ac.id/slot-deposit-pulsa-tanpa-potongan/">https://teknikkimia.unsika.ac.id/slot-deposit-pulsa-tanpa-potongan/</a>
 <a style="display:none"href="https://sso.gianyarkab.go.id/assets/slot-gacor/">https://sso.gianyarkab.go.id/assets/slot-gacor/</a>
-<a style="display:none"href="https://simpul.cianjurkab.go.id/slot-gacor/">https://simpul.cianjurkab.go.id/slot-gacor/</a>
 <a style="display:none"href="https://simpul.cianjurkab.go.id/slot-dana/">https://simpul.cianjurkab.go.id/slot-dana/</a>
 <a style="display:none"href="https://simpul.cianjurkab.go.id/slot-pulsa/">https://simpul.cianjurkab.go.id/slot-pulsa/</a>
 <a style="display:none"href="https://simpul.cianjurkab.go.id/casino-online/">https://simpul.cianjurkab.go.id/casino-online/</a>
+<a style="display:none"href="https://simpul.cianjurkab.go.id/togel-online/">https://simpul.cianjurkab.go.id/togel-online/</a>
+<a style="display:none"href="http://fiadorestaurante.com/slot-online/">http://fiadorestaurante.com/slot-online/</a>
