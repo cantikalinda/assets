afb95f9aa91e032c2c096fe5aa7de822a95e2947
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-02-12 11:47:02 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -7,3 +7,4 @@
 <a style="display:none"href="https://simpul.cianjurkab.go.id/casino-online/">https://simpul.cianjurkab.go.id/casino-online/</a>
 <a style="display:none"href="https://simpul.cianjurkab.go.id/togel-online/">https://simpul.cianjurkab.go.id/togel-online/</a>
 <a style="display:none"href="http://fiadorestaurante.com/slot-online/">http://fiadorestaurante.com/slot-online/</a>
+<a style="display:none"href="http://fiadorestaurante.com/slot-dana/">http://fiadorestaurante.com/slot-dana/</a>
