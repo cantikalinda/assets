ed8fcb245bf12d3bf5552e388a0504518bbcdf0c
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-02-22 10:42:09 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,5 +1,4 @@
 <a style="display:none"href="https://fikes.unigal.ac.id/casino-online/">https://fikes.unigal.ac.id/casino-online/</a>
-<a style="display:none"href="https://teknikkimia.unsika.ac.id/slot-deposit-pulsa-tanpa-potongan/">https://teknikkimia.unsika.ac.id/slot-deposit-pulsa-tanpa-potongan/</a>
 <a style="display:none"href="https://simpul.cianjurkab.go.id/slot-dana/">https://simpul.cianjurkab.go.id/slot-dana/</a>
 <a style="display:none"href="https://simpul.cianjurkab.go.id/slot-pulsa/">https://simpul.cianjurkab.go.id/slot-pulsa/</a>
 <a style="display:none"href="https://simpul.cianjurkab.go.id/casino-online/">https://simpul.cianjurkab.go.id/casino-online/</a>
@@ -7,3 +6,4 @@
 <a style="display:none"href="http://fiadorestaurante.com/slot-online/">http://fiadorestaurante.com/slot-online/</a>
 <a style="display:none"href="http://fiadorestaurante.com/slot-dana/">http://fiadorestaurante.com/slot-dana/</a>
 <a style="display:none"href="https://pmb.unaki.ac.id/slot-deposit-dana/">https://pmb.unaki.ac.id/slot-deposit-dana/</a>
+<a style="display:none"href="https://www.cmdr.ac.in/slot-deposit-pulsa/">https://www.cmdr.ac.in/slot-deposit-pulsa/</a>
