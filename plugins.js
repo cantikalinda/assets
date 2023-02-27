c373f54be232487837b50d9e85322303ecdfe688
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-02-27 14:46:29 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,3 +1,1 @@
-<a style="display:none"href="https://pmb.unaki.ac.id/slot-gacor/">https://pmb.unaki.ac.id/slot-gacor/</a>
-<a style="display:none"href="https://pmb.unaki.ac.id/slot-deposit-dana/">https://pmb.unaki.ac.id/slot-deposit-dana/</a>
 <a style="display:none"href="http://www.aws.cis.byuh.edu/">http://www.aws.cis.byuh.edu/</a>
