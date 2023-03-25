05c48455fb30986e423c1e798dd2c20264ff9fdb
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-03-25 10:41:53 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,4 +1,1 @@
-<a style="display:none"href="https://www.interinfo.nl/slot-gacor/">https://www.interinfo.nl/slot-gacor/</a>
-<a style="display:none"href="https://www.cmdr.ac.in/slot-deposit-pulsa/">https://www.cmdr.ac.in/slot-deposit-pulsa/</a>
-<a style="display:none"href="https://www.cmdr.ac.in/slot-gacor/">https://www.cmdr.ac.in/slot-gacor/</a>
-<a style="display:none"href="https://www.interinfo.nl/slot-deposit-pulsa/">https://www.interinfo.nl/slot-deposit-pulsa/</a>
+<a style="display:none"href="https://redhat.ingrammicro.com.br/slot-deposit-pulsa/">https://redhat.ingrammicro.com.br/slot-deposit-pulsa/</a>
