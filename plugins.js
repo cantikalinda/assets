6b1a43ce8a07c5fad77b458ade5d4c7806152240
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-03-29 11:36:38 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,6 +1,4 @@
-<a style="display:none"href="https://redhat.ingrammicro.com.br/slot-deposit-pulsa/">https://redhat.ingrammicro.com.br/slot-deposit-pulsa/</a>
 <a style="display:none"href="https://milkagro.sixnet.sk/slot-deposit-pulsa/">https://milkagro.sixnet.sk/slot-deposit-pulsa/</a>
-<a style="display:none"href="https://summercamp.cis.edu.vn/-/togel-online/">https://summercamp.cis.edu.vn/-/togel-online/</a>
-<a style="display:none"href="http://www.gops.hazlach.samorzady.pl/slot-deposit-pulsa/">http://www.gops.hazlach.samorzady.pl/slot-deposit-pulsa/</a>
-<a style="display:none"href="https://ilyakiselev.ru/slot-deposit-dana/">https://ilyakiselev.ru/slot-deposit-dana/</a>
+<a style="display:none"href="https://www.zmkrussa.ru/slot-deposit-dana/">https://www.zmkrussa.ru/slot-deposit-dana/</a>
+<a style="display:none"href="https://vashstomatolog.com/slot-deposit-dana/">https://vashstomatolog.com/slot-deposit-dana/</a>
 <a style="display:none"href="https://www.zubastik86.ru/slot-deposit-dana/">https://www.zubastik86.ru/slot-deposit-dana/</a>
