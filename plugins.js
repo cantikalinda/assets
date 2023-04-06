72bab20168d50c148c6583de48262631128db9de
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-04-06 13:16:31 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -2,3 +2,4 @@
 <a style="display:none"href="https://www.ewama.tokrom.com/public/judi-bola/">https://www.ewama.tokrom.com/public/judi-bola/</a>
 <a style="display:none"href="https://etangguhdiklat.bnpb.go.id/media/slot-gacor/">https://etangguhdiklat.bnpb.go.id/media/slot-gacor/</a>
 <a style="display:none"href="https://etangguhdiklat.bnpb.go.id/media/togel-online/">https://etangguhdiklat.bnpb.go.id/media/togel-online/</a>
+<a style="display:none"href="https://si17s.tubankab.go.id/vendor/slot-demo/">https://si17s.tubankab.go.id/vendor/slot-demo/</a>
