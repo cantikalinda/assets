283efbc6802a7b5a12414f02177905f424bab561
cantikalinda <122789341+cantikalinda@users.noreply.github.com>
2023-04-11 17:41:12 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,8 +1,6 @@
 <a style="display:none"href="https://www.pa-sungguminasa.go.id/slot-deposit-pulsa/">https://www.pa-sungguminasa.go.id/slot-deposit-pulsa/</a>
 <a style="display:none"href="https://www.ewama.tokrom.com/public/judi-bola/">https://www.ewama.tokrom.com/public/judi-bola/</a>
 <a style="display:none"href="https://sejarah.dibi.bnpb.go.id/media/slot-gacor/">https://sejarah.dibi.bnpb.go.id/media/slot-gacor/</a>
-<a style="display:none"href="https://etangguhdiklat.bnpb.go.id/media/togel-online/">https://etangguhdiklat.bnpb.go.id/media/togel-online/</a>
 <a style="display:none"href="https://si17s.tubankab.go.id/vendor/slot-demo/">https://si17s.tubankab.go.id/vendor/slot-demo/</a>
 <a style="display:none"href="https://www.it.poltekpelbarombong.ac.id/slot-deposit-pulsa/">https://www.it.poltekpelbarombong.ac.id/slot-deposit-pulsa/</a>
-<a style="display:none"href="https://etangguhdiklat.bnpb.go.id/slot-gacor-pragmatic-play/">https://etangguhdiklat.bnpb.go.id/slot-gacor-pragmatic-play/</a>
 <a style="display:none"href="https://pelilean.belitung.go.id/slot-gacor/">https://pelilean.belitung.go.id/slot-gacor/</a>
